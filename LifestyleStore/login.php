07752a403f316dab63c36e701ef339ce29c58143
Liyaaana <90127244+Liyaaana@users.noreply.github.com>
2023-08-05 23:47:26 +0530

    Update login.php

--- LifestyleStore/login.php
+++ LifestyleStore/login.php
@@ -54,7 +54,7 @@
            <footer class="footer">
                <div class="container">
                <center>
-                   <p>Copyright &copy Lifestyle Store. All Rights Reserved. | Contact Us: +91 90000 00000</p>
+                   <p>Copyright &copy Campentre. All Rights Reserved. | Contact Us: +91 90000 00000</p>
                    <p>This website is developed by Sajal Agrawal</p>
                </center>
                </div>
