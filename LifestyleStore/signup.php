4b62fc8c00e91891b61388e9d2dcd934c2f99d07
Liyaaana <90127244+Liyaaana@users.noreply.github.com>
2023-08-06 00:07:04 +0530

    Update signup.php

--- LifestyleStore/signup.php
+++ LifestyleStore/signup.php
@@ -9,8 +9,8 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="shortcut icon" href="img/lifestyleStore.png" />
-        <title>Lifestyle Store</title>
+        <link rel="shortcut icon" href="img/Campentre.png" />
+        <title>Campentre</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <!-- latest compiled and minified CSS -->
@@ -62,8 +62,8 @@
            <footer class="footer">
                <div class="container">
                <center>
-                   <p>Copyright &copy Lifestyle Store. All Rights Reserved. | Contact Us: +91 90000 00000</p>
-                   <p>This website is developed by Sajal Agrawal</p>
+                   <p> Contact Us: +91 90000 00000</p>
+                  
                </center>
                </div>
            </footer>
